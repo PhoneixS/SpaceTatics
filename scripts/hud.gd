a36efad6dc0d01241e380ccaf91da2838ccb8d07
Javier Alfonso <PhoneixSegovia@gmail.com>
2025-06-28 12:17:52 +0200

    Show species name (from enum key).

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -7,11 +7,14 @@ var turn_manager: TurnManager
 
 func _on_ready() -> void:
 	self.turn_manager = get_parent().get_node("TurnManager")
-	($MarginContainer/HBTop/PlayerNick as Label).text = self.turn_manager.get_current_player().nick
+	($MarginContainer/HBTop/PlayerNick as Label).text = _generate_player_label_text(self.turn_manager.get_current_player())
 	self.turn_manager.player_changed.connect(_on_turn_manager_player_changed)
 
 func _on_end_turn_pressed() -> void:
 	self.turn_ended.emit()
 
 func _on_turn_manager_player_changed(_oldPlayer: Player, newPlayer: Player, _idx: int) -> void:
-	($MarginContainer/HBTop/PlayerNick as Label).text = newPlayer.nick + "-" + str(newPlayer.species)
+	($MarginContainer/HBTop/PlayerNick as Label).text = _generate_player_label_text(newPlayer)
+
+func _generate_player_label_text(player: Player) ->  String:
+	return player.nick + " - " + str(SkirmisInfo.Species.find_key(player.species))
