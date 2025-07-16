a92facff588441ca40969ca71f705bfc6a80f51f
Javier Alfonso <PhoneixSegovia@gmail.com>
2025-07-16 22:13:21 +0200

    Implemented basic zooming of the camera.

--- scripts/main_battle_camera.gd
+++ scripts/main_battle_camera.gd
@@ -1,7 +1,14 @@
 extends Camera2D
 
 @export var speed = 10
+@export var zoom_speed = 1.1
 
 func _input(event: InputEvent) -> void:
 	var direction: Vector2 = Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
-	self.position += direction * speed
+	self.position += direction * self.speed
+	
+	if event.is_action("zoom_in"):
+		print("zooming in!!")
+		self.zoom = self.zoom * self.zoom_speed
+	elif  event.is_action("zoom_out"):
+		self.zoom = self.zoom / self.zoom_speed
