934aea2f5f711f1232213078a45bf22b398a979a
Javier Alfonso <PhoneixSegovia@gmail.com>
2025-07-16 22:21:37 +0200

    Added player color to the HUD.

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -7,14 +7,19 @@ var turn_manager: TurnManager
 
 func _on_ready() -> void:
 	self.turn_manager = get_parent().get_node("TurnManager")
-	($MarginContainer/HBTop/PlayerNick as Label).text = _generate_player_label_text(self.turn_manager.get_current_player())
+	self._set_player_properties(self.turn_manager.get_current_player())
 	self.turn_manager.player_changed.connect(_on_turn_manager_player_changed)
 
 func _on_end_turn_pressed() -> void:
 	self.turn_ended.emit()
 
 func _on_turn_manager_player_changed(_oldPlayer: Player, newPlayer: Player, _idx: int) -> void:
-	($MarginContainer/HBTop/PlayerNick as Label).text = _generate_player_label_text(newPlayer)
+	self._set_player_properties(newPlayer)
+	
 
 func _generate_player_label_text(player: Player) ->  String:
 	return player.nick + " - " + str(SkirmisInfo.Species.find_key(player.species))
+
+func _set_player_properties(player: Player) -> void:
+	($MarginContainer/HBTop/PlayerNick as Label).text = _generate_player_label_text(player)
+	($MarginContainer/HBTop/PlayerColor as ColorRect).color = player.color
